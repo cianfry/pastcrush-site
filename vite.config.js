973fbbc9d1e4781fa4e3ee011f241ae536db4953
Francesca Ribezzi <f.ribezzi1@gmail.com>
2025-10-28 21:22:39 +0100

    add SEO and other stuff

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+
 
 // 🧭 Update this only if your GitHub repo name changes
 // For example, if your repo is github.com/cianfry/pastcrush
 // then `base: "/pastcrush/"` ensures correct asset paths on GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: "/pastcrush/",
+  base: '/', // ✅ important for custom domains like pastcrush.shop
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+   }
 });
+
